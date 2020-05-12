9868e00136aebb30a2e42efc3ac6e4ab0c81a369
hegemon76 <adrian.dawid76@gmail.com>
2020-05-12 12:35:25 +0200

    poprawne wyswietlanie pytan

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Question;
+use App\Repository\QuestionRepository;
 
 class AdminPanelController extends AbstractController
 {
     /**
      * @Route("/admin-panel", name="Panel Administracyjny")
      */
-    public function panel()
+    public function panel(QuestionRepository $questionsRepository)
     {
-        return $this->render('admin-panel/admin-panel.html.twig', [
+        return $this->render('admin-panel/admin2.html.twig', [
             'controller_name' => 'AdminPanelController',
+            'questions' => $questionsRepository->findAll(),
         ]);
     }
 }
