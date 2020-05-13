16bc07ac5023ebd81608ecfab1e457d3727e2fa2
hegemon76 <adrian.dawid76@gmail.com>
2020-05-13 11:29:33 +0200

    drobne poprawki

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Question;
 use App\Repository\QuestionRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\BrowserKit\Request;
+use App\Form\DoctorType;
 
 class AdminPanelController extends AbstractController
 {
@@ -17,6 +20,31 @@ class AdminPanelController extends AbstractController
         return $this->render('admin-panel/admin2.html.twig', [
             'controller_name' => 'AdminPanelController',
             'questions' => $questionsRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin_add_doctor", name="dodaj_doktora")
+     */
+    public function index(EntityManagerInterface $em, Request $request)
+    {
+        $form = $this->createForm(DoctorType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $question = $form->getData();
+
+            $em->persist($question);
+            $em->flush();
+
+            return $this->redirectToRoute('Panel Administracyjny');
+        }
+
+        return $this->render('admin-panel/add_doctor.html.twig', [
+            'controller_name' => 'AdminPanelController',
+            'questionForm' => $form->createView(),
+
         ]);
     }
 }
