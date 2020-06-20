301a85ca186faca8ed8db82b820211029ec7852e
hegemon76 <adrian.dawid76@gmail.com>
2020-06-20 10:23:24 +0200

    dodany crud doctora

---
+++ src/Controller/DoctorController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Doctor;
+use App\Form\Doctor1Type;
+use App\Repository\DoctorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/doctor")
+ */
+class DoctorController extends AbstractController
+{
+    /**
+     * @Route("/", name="doctor_index", methods={"GET"})
+     */
+    public function index(DoctorRepository $doctorRepository): Response
+    {
+        return $this->render('doctor/index.html.twig', [
+            'doctors' => $doctorRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="doctor_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $doctor = new Doctor();
+        $form = $this->createForm(Doctor1Type::class, $doctor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($doctor);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('doctor_index');
+        }
+
+        return $this->render('doctor/new.html.twig', [
+            'doctor' => $doctor,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="doctor_show", methods={"GET"})
+     */
+    public function show(Doctor $doctor): Response
+    {
+        return $this->render('doctor/show.html.twig', [
+            'doctor' => $doctor,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="doctor_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Doctor $doctor): Response
+    {
+        $form = $this->createForm(Doctor1Type::class, $doctor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('doctor_index');
+        }
+
+        return $this->render('doctor/edit.html.twig', [
+            'doctor' => $doctor,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="doctor_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Doctor $doctor): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$doctor->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($doctor);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('doctor_index');
+    }
+}
